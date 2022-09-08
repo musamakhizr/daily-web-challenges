12dc30c59d5cb39a490e84beacdb20018b81efd4
MUsamaKhizar <usamakhizar07@gmail.com>
2022-09-08 09:03:01 -0700

    Challenge 09/08/2022 - Counter Problem

---
+++ Javascript/Function-Constructor/constructor-fun.js
@@ -0,0 +1,12 @@
+//Create a counter whihc we can use globally as and can increment and decrement it
+function counter()
+{
+    var count = 0;
+
+   //Place Code Here
+
+   console.log(count);
+}
+
+var counter1 ;
+
